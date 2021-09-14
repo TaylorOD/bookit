56f801a68cfb4bb0149c0dfa7f1833ba4bd45a96
taylorod <taylorsunwantedemails@gmail.com>
2021-09-14 09:20:08 -0400

    feat: :sparkles: Add more redux code

--- redux/reducers/reducers.js
+++ redux/reducers/reducers.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
+import { allRoomsReducer } from "./roomRecucers";
 
 const reducer = combineReducers({
-
+  allRooms: allRoomsReducer
 })
 
 export default reducer
