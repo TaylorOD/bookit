3e17882a4d277d83c1058fd4fa347ecbfd8691c8
taylorod <taylorsunwantedemails@gmail.com>
2021-11-01 13:15:36 -0400

    add avatar saving for users

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,20 +1,35 @@
 import User from "../models/user"
+import Cloudinary from "cloudinary"
 
 import ErrorHandler from "../utils/errorHandler"
 import catchAsyncErrors from "../middlewares/catchAsyncErrors"
 import APIFeatures from "../utils/apiFeatures"
 
+// Setting up cloudinary config
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
 // Register User => /api/auth/register
 const registerUser = catchAsyncErrors(async (req, res) => {
   const { name, email, password } = req.body
+
+  const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    folder: "bookit/avatars",
+    width: "150",
+    crop: "scale"
+  })
 
   const user = await User.create({
     name,
     email,
     password,
     avatar: {
-      public_id: "PUBLIC_ID",
-      url: "URL"
+      public_id: result.public_id,
+      url: result.secure_url
     }
   })
   res.status(200).json({
--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   env: {
-    DB_LOCAL_URI: "mongodb://localhost:27017/bookit"
+    DB_LOCAL_URI: "mongodb://localhost:27017/bookit",
+
+    CLOUDINARY_CLOUD_NAME: "ds8otfoya",
+    CLOUDINARY_API_KEY: "692982392269691",
+    CLOUDINARY_API_SECRET: "uG35YIRBIZQhwRp0MCD5_gcyXLs",
   },
   images: {
-    domains: ["res.cloudinary.com"]
+    domains: ["res.cloudinary.com"],
   },
   reactStrictMode: true,
 }
